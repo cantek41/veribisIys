80e78f813b887c675e9a31f198e00e1196b3ca48
fsahinbas <fsahinbas@gmail.com>
2015-04-05 17:14:15 +0300

    admin layoutları gruplandi

--- iys/iys/Scripts/customScript.js
+++ iys/iys/Scripts/customScript.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
 
     // Adds title attributes and classnames to list items
-
+    $("ul#nav li a:contains('er')").addClass("settings").attr('title', 'Eğitim İçeriği');
     $("ul#nav li a:contains('Kurslar')").addClass("dashboard").attr('title', 'Kurslar');
     $("ul#nav li a:contains('mler')").addClass("pages").attr('title', 'Bölümler');
     $("ul#nav li a:contains('Dersler')").addClass("assets").attr('title', 'Dersler');
@@ -9,6 +9,7 @@
     $("ul#nav li a:contains('Sorular')").addClass("widgets").attr('title', 'Sorular');
     $("ul#nav li a:contains('Ekle')").addClass("search").attr('title', 'Cevap Ekle');
     $("ul#nav li a:contains('Sonucu')").addClass("trash").attr('title', 'Cevap Sonucu');
+    $("ul#nav li a:contains('Anasayfa')").addClass("dashboard").attr('title', 'Anasayfa');
 
     // Animate sidebar navigation
 
